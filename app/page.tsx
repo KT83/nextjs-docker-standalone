1e13318c97c70cc00979f5c78d47ecf96ae3b8e1
Keita Mitsuhashi <keita.mitsuhashi.83@gmail.com>
2025-04-16 13:55:48 +0900

    update: chat app demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
+"use client";
+
+import { ChatForm } from "@/components/chat-form";
+import { ChatMessages } from "@/components/chat-messages";
+import { MessageArea } from "@/components/message-area";
+import { useChat } from "@ai-sdk/react";
+
 export default function Home() {
+  const { messages, input, handleInputChange, handleSubmit, status } = useChat({
+    api: "/api/chat",
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl">Hello World! Next.js is running on Morph</h1>
-      </main>
-    </div>
+    <main
+      className={`
+  min-h-screen p-12
+  flex flex-col items-stretch justify-center 
+  prose max-w-none prose-sm prose-p:my-0
+`}
+    >
+      {messages.length === 0 && (
+        <div className="mb-2 flex flex-col items-center">
+          <h2 className="text-3xl font-bold">How Can I help you??</h2>
+        </div>
+      )}
+      {messages.length > 0 && (
+        <MessageArea status={status} messages={messages} className="flex-1">
+          <ChatMessages messages={messages} />
+        </MessageArea>
+      )}
+      <ChatForm
+        inputValue={input}
+        onInputChange={handleInputChange}
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("submit");
+          handleSubmit(e);
+        }}
+      />
+    </main>
   );
 }
